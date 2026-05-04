6a278756de6df5c6c6e6a36251dbc8dc2b64b20e
Tejas Ulawekar <Tejasu@mkcl.org>
2026-05-04 17:27:32 +0530

    Added Delete and View

--- frontend/vite-project/src/router/index.ts
+++ frontend/vite-project/src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import StudentList from "../pages/StudentList.vue"
 import AddStudent from "../pages/AddStudent.vue"
+import ViewStudent from "../pages/viewStudent.vue"
 
 const routes = [
   { path: "/", component: StudentList, alias: "/students" },
-  { path: "/add", component: AddStudent }
+  { path: "/add", component: AddStudent },
+  {
+    path: "/students/:id",
+    name: "view-student",
+    component: () => ViewStudent,
+    props: true
+  }
 ]
 
 export const router = createRouter({
--- frontend/vite-project/src/services/api.ts
+++ frontend/vite-project/src/services/api.ts
@@ -3,5 +3,10 @@ import axios from "axios";
 const Api = "http://localhost:8080/api";
 
 export const getStudents = async () => axios.get(`${Api}/students`);
+export const getStudentById = async (id: number) => {
+  const res = await axios.get(`${Api}/students/${id}`)
+  return res.data
+}
 export const addStudent = (data: any) => axios.post(`${Api}/students`, data);
-export const runCap = () => axios.post(`${Api}/run-cap`);
+export const runCap = () => axios.post(`${Api}/run-cap`)
+export const deleteStudent = (id: number) => axios.delete(`${Api}/students/${id}`);
